636268168e2c996407053bde3f79cc3e420522ac
Pablo Damián Alcaraz <pablodalcaraz@gmail.com>
2024-06-05 14:53:27 -0300

    vender pasaje  - Pasaje Data

--- SolBusGrupo15Laboratorio/src/accesoDatos/PasajeData.java
+++ SolBusGrupo15Laboratorio/src/accesoDatos/PasajeData.java
@@ -1,6 +1,46 @@
 
 package accesoDatos;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+import javax.swing.JOptionPane;
+import solbusgrupo15laboratorio.entidades.Pasaje;
+
 public class PasajeData {
+    private Connection con = null;
+
+    public PasajeData() {
+        con = Conexion.getConexion();
+    }
     
+    public void venderPasaje(Pasaje pasaje){
+        String sql = "INSERT INTO pasaje(  `idPasajero`, `idColectivo`, `idRuta`, `fechaviaje`, `horaViaje`, `asiento`, `precio`) VALUES (?, ?, ?, ?, ?, ?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setInt(1,pasaje.getIdPasajero() );
+            ps.setInt(2, pasaje.getIdColectivo());
+            ps.setInt(3, pasaje.getIdRuta());
+            ps.setDate(4, Date.valueOf(pasaje.getFechaViaje()));
+            ps.setTime(5, Time.valueOf(pasaje.getHoraViaje()));
+            ps.setInt(6, pasaje.getAsiento());
+            ps.setDouble(7, pasaje.getPrecio());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+                pasaje.setIdPasaje(rs.getInt(1));;
+                JOptionPane.showMessageDialog(null, "Pasaje añadido con exito.");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Pasaje" + ex.getMessage());
+        }
+    }
 }
--- SolBusGrupo15Laboratorio/src/solbusgrupo15laboratorio/SolBusGrupo15Laboratorio.java
+++ SolBusGrupo15Laboratorio/src/solbusgrupo15laboratorio/SolBusGrupo15Laboratorio.java
@@ -3,28 +3,35 @@ package solbusgrupo15laboratorio;
 
 import accesoDatos.PasajeroData;
 import accesoDatos.RutaData;
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 import solbusgrupo15laboratorio.entidades.Pasajero;
 import solbusgrupo15laboratorio.entidades.Ruta;
+import solbusgrupo15laboratorio.entidades.Pasaje;
+import accesoDatos.PasajeData;
 
 public class SolBusGrupo15Laboratorio {
 
     public static void main(String[] args) {
 //        Ruta r1 = new Ruta(1,"La Punta", "San Luis",LocalTime.of(1, 20) , true);
+//          Ruta r2 = new Ruta(1,"La Punta", "San Luis",LocalTime.of(1, 20) , true);
 //        RutaData rData = new RutaData();
-//        rData.aniadirNuevaRuta(r1);
-        Pasajero p1 = new Pasajero(1, "Josias", "Tolaba", 12345678, "josiastolaba@gmail.com", 266412345, true);
-        PasajeroData pasajeroData = new PasajeroData();
-//        pasajeroData.guardarPasajero(p1);
-        List<Pasajero> pasajeros = new ArrayList<>();
-        pasajeros = pasajeroData.listarPasajero();
-        for (Pasajero aux:pasajeros){ 
-        System.out.println(aux.toString());
+////        rData.aniadirNuevaRuta(r1); 
+//          rData.aniadirNuevaRuta(r2);
+//        Pasajero p1 = new Pasajero(1, "Josias", "Tolaba", 12345678, "josiastolaba@gmail.com", 266412345, true);
+//        PasajeroData pasajeroData = new PasajeroData();
+////        pasajeroData.guardarPasajero(p1);
+//        List<Pasajero> pasajeros = new ArrayList<>();
+//        pasajeros = pasajeroData.listarPasajero();
+//        for (Pasajero aux:pasajeros){ 
+//        System.out.println(aux.toString());
+          Pasaje pas1= new Pasaje(1, 1, 1, 1, LocalDate.of(2024, Month.JUNE, 4), LocalTime.of(12, 05), 3, 750);
+          PasajeData pasData=new PasajeData();
+          pasData.venderPasaje(pas1);
         
     }
-        
-    }
-    
+     
 }

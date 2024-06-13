666a5b99936fca39e63599d613a12a27238bbf88
Pablo Damián Alcaraz <pablodalcaraz@gmail.com>
2024-06-13 20:42:18 -0300

    icono eliminar ruta

--- SolBusGrupo15Laboratorio/src/vistas/GestionRuta.java
+++ SolBusGrupo15Laboratorio/src/vistas/GestionRuta.java
@@ -57,8 +57,8 @@ public class GestionRuta extends javax.swing.JInternalFrame {
         jTOrigen = new javax.swing.JTextField();
         jTDestino = new javax.swing.JTextField();
         jREstado = new javax.swing.JRadioButton();
-        jCHora = new javax.swing.JComboBox<Integer>();
-        jCMinutos = new javax.swing.JComboBox<Integer>();
+        jCHora = new javax.swing.JComboBox<>();
+        jCMinutos = new javax.swing.JComboBox<>();
         jLabel11 = new javax.swing.JLabel();
         jLabel12 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
@@ -290,10 +290,10 @@ public class GestionRuta extends javax.swing.JInternalFrame {
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jTDestino2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel9)))
-                    .addComponent(jBBuscar, javax.swing.GroupLayout.DEFAULT_SIZE, 85, Short.MAX_VALUE))
+                    .addComponent(jBBuscar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 14, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                         .addComponent(jLabel5)
@@ -379,7 +379,7 @@ public class GestionRuta extends javax.swing.JInternalFrame {
             }
         });
 
-        jBBorrar.setText("Borrar");
+        jBBorrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/eliminar.png"))); // NOI18N
         jBBorrar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jBBorrarActionPerformed(evt);
@@ -404,9 +404,9 @@ public class GestionRuta extends javax.swing.JInternalFrame {
                             .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                                 .addGap(378, 378, 378)
                                 .addComponent(jBLimpiar2)
-                                .addGap(18, 18, 18)
-                                .addComponent(jBBorrar)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jBBorrar)
+                                .addGap(145, 145, 145)
                                 .addComponent(jBSalir))
                             .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
